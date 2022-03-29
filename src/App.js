0d2b708393e28f26b8585e363e895eea75299bf6
Anastasia811625 <86957944+Anastasia811625@users.noreply.github.com>
2022-03-29 08:36:24 +0300

    added ExchangeRates into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './styles/App.css';
 
+import ExchangeRates from './components/ExchangeRates';
+
 const App = () => {
   return (
     <>
+      <ExchangeRates/>
     </>
   )
 }
